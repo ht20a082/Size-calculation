a21cca38c08999c36c9f85a81fd70f64de9a9772
KeitoH <ht20a082@oecu.jp>
2022-07-11 14:15:17 +0900

    Fix

--- input.py
+++ input.py
@@ -1,31 +1,31 @@
 import cv2
 
 capture = cv2.VideoCapture(0)
-ret, frame = capture.read()
 
 tracker = cv2.TrackerMIL_create()
+success, img = capture.read()
 
-bbox = cv2.selectROI("Tracking", frame, False)
-tracker.init(frame, bbox)
+bbox = cv2.selectROI("Tracking", img, False)
+tracker.init(img, bbox)
 font = cv2.FONT_HERSHEY_SIMPLEX
 
-def drawBox(frame, bbox):
+def drawBox(img, bbox):
     x, y, w, h = int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3])
-    cv2.rectangle(frame, (x, y), ((x+w), (y+h)), (255, 0, 0), 3, 1)
-    cv2.putText(frame, 'Tracking', (15, 70), font, 0.5, (0, 0, 255), 2)
+    cv2.rectangle(img, (x, y), ((x+w), (y+h)), (255, 0, 0), 3, 1)
+    cv2.putText(img, 'Tracking', (15, 70), font, 0.5, (0, 0, 255), 2)
 
 while (True):
     timer = cv2.getTickCount()
-    ret, frame = capture.read()
+    sccess, img = capture.read()
 
-    ret, bbox = tracker.update(frame)
+    success, bbox = tracker.update(img)
 
-    if ret:
-        drawBox(frame, bbox)
+    if success:
+        drawBox(img, bbox)
     else:
-        cv2.putText(frame, 'Tracking Lost', (15, 70), font, 0.5, (0,0,255),2)
+        cv2.putText(img, 'Tracking Lost', (15, 70), font, 0.5, (0,0,255),2)
 
-    cv2.imshow('frame', frame)
+    cv2.imshow('frame', img)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
